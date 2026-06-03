b97c2c4d53c4c11c9ce61cdce8bde1602d1e96c2
Saarthi Basant Gupta <saarthibasantgupta31@gmail.com>
2026-06-03 20:56:29 +0530

    testing

--- notes.py
+++ notes.py
@@ -1,1 +1,15 @@
-# test commit   
+import json
+
+notes = []
+while True:
+    note_text = input("Write a note (or 'done'): ")
+
+    if note_text == "done":
+        break
+
+    notes.append(
+        {
+        "text": note_text
+        })
+with open("notes.json", "w") as file:
+    json.dump(notes, file, indent=4)
