3294a2798812523d6da2b6d19f6bf15d85a8c29f
Saarthi Basant Gupta <saarthibasantgupta31@gmail.com>
2026-06-17 20:44:12 +0530

    Adding "Edit Note" Function

--- notes_functions.py
+++ notes_functions.py
@@ -7,3 +7,35 @@ def view_notes(notes):
     for i, note in enumerate(notes, start=1):
         print(f"\n{i}. {note['title']}")
         print(note["text"])
+
+def edit_note(notes):
+    if len(notes) ==0:
+        print("No notes found")
+        return
+    
+    view_notes(notes)
+
+    try:
+        choice = int(input("Enter note number to edit: ")) -1
+        
+        if 0<= choice<len(notes):
+            new_title = input("New title(leave bank to keep current)")
+            new_text = input("New text(leave blank to keep current)")
+
+            if new_title:
+                notes[choice]["title"] = new_title
+            
+            if new_text:
+                notes[choice]["text"] = new_text
+
+            with open("notes.json", "w") as file:
+                json.dump(notes,file,indent=4)
+
+            print("Note updated")
+
+        else:
+            print("Invalid note number")
+    except: 
+        print("Please enter a valid note number")
+
+        
